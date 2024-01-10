ce4af7bbd95c4bfaa31a30e3f166913549579cd5
Serchinastico <github@serchinastico.com>
2024-01-10 01:10:56 +0100

    Create the development flow, building the template with EJS and using tailwind for styling

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,53 @@
-import { Command, OptionValues } from "commander";
-import exec from "exec-sh";
+import { Command } from "commander";
 import figlet from "figlet";
 import "dotenv/config";
 import ejs from "ejs";
 import fs from "fs";
+import { graphql } from "@octokit/graphql";
+import { User } from "@octokit/graphql-schema";
 
 const create = async (username: string, projectId: string) => {
   const { output: outFile } = program.opts();
 
   const template = fs.readFileSync("template/index.ejs.html", "utf-8");
-  const contents = ejs.render(template, {});
+
+  const { user } = await graphql<{ user: User }>(
+    `
+      {
+        user(login: "${username}") {
+          projectV2 (number: ${projectId}) {
+            title
+            shortDescription
+            items(first: 50) {
+              nodes {
+                content {
+                  ... on DraftIssue {
+                    id
+                    title
+                  }
+                  ... on Issue {
+                    id
+                    title
+                    url
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    `,
+    {
+      headers: {
+        authorization: `token ${process.env.GH_TOKEN}`,
+      },
+    }
+  );
+
+  const contents = ejs.render(template, {
+    title: user.projectV2?.title,
+    description: user.projectV2?.shortDescription,
+  });
 
   if (outFile) {
     fs.writeFileSync(outFile, contents);
@@ -30,10 +68,10 @@ program
   )
   .command("create")
   .argument("<username>", "The GitHub username of the owner of the project")
-  .argument("<project id>", "The project id (usually a number)")
+  .argument("<project id>", "The project id (number)")
   .action(create);
 
 program
   .option("-d, --debug", "Prints debug information")
-  .option("-o, --output <file>", "Output HTML file")
+  .option("-o, --output <file>", "Output HTML file", "out/index.html")
   .parse(process.argv);
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./template/**/*.html", "./out/**/*.html"],
+  theme: {
+    colors: {
+      ash: "#1E1F25",
+      white: "#FFFFFF",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
